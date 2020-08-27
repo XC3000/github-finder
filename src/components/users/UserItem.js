546c084ed8571599418be5973b6ead68575f172c
XC3000 <richardrozario.rr@gmail.com>
2020-08-27 12:13:45 +0530

    initial

---
+++ src/components/users/UserItem.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const UserItem = (props) => {
+  /* state = {
+    id: 1,
+    login: "mojombo",
+    avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
+    url: "https://api.github.com/users/mojombo",
+    html_url: "https://github.com/mojombo",
+  }; */
+  const { login, avatar_url, html_url } = props.user;
+
+  return (
+    <div className="card text-center">
+      <img
+        src={avatar_url}
+        alt=""
+        className="round-img"
+        style={{ width: "60px" }}
+      />
+      <h3>{login}</h3>
+      <div>
+        <a href={html_url} className="btn btn-dark btn-sm my-1">
+          More
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default UserItem;
