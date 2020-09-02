dc0c169cc6ba744a554d3af03a4e9c4774298f4b
XC3000 <richardrozario.rr@gmail.com>
2020-09-03 00:58:08 +0530

    added react router dom

---
+++ src/components/pages/About.js
@@ -0,0 +1,17 @@
+import React, { Fragment } from "react";
+
+const About = () => {
+  return (
+    <Fragment>
+      <h1>About Page</h1>
+      <p>App to search GitHub Users</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi
+        distinctio ab ipsum officiis commodi sunt recusandae enim quo velit
+        odit. Quaerat dolores culpa ut aperiam earum in vitae laborum nam!
+      </p>
+    </Fragment>
+  );
+};
+
+export default About;
